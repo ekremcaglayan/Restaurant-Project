cd1d94f826ffff594a86884985f3d7dcf65d078c
semdin <mehmetsemdinaktay@gmail.com>
2023-04-16 17:22:01 +0300

    passing ejs

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+
+app.use(express.urlencoded({
+    extended: false
+  })); // Not using bodyParser, using Express in-built body parser instead
+  app.set("view engine", "ejs");
+  app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+  res.render('anasayfa', {title: 'Anasayfa'});
+});
+
+app.listen(3000, () => {
+  console.log('Server has been started on port 3000.');
+});
